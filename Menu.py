8ca65fc684aecc07063f630c237f213316f7ae3d
Falanan <wenbo.zhang1@ontariotechu.net>
2024-11-15 22:02:14 -0500

    Update some progress

--- Menu.py
+++ Menu.py
@@ -1,3 +1,18 @@
 def main():
-    pass
+    print("Please select your language:")
+    print("1. English")
+    print("2. Chinese")
 
+    while True:
+        choice = input("Enter your choice (1 or 2): ")
+        if choice == '1':
+            print("You selected English.")
+            return "English"
+        elif choice == '2':
+            print("You selected Chinese.")
+            return "Chinese"
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+if __name__ == "__main__":
+    main()
--- private_assistant.py
+++ private_assistant.py
@@ -2,7 +2,7 @@ import sys
 import os
 import contextlib
 
-from chatglm.glm4_module import GLMChatbot
+# from chatglm.glm4_module import GLMChatbot
 
 _stdout_backup = sys.stdout
 _stderr_backup = sys.stderr
@@ -26,19 +26,75 @@ def toggle_output(enable=True):
         sys.stderr = open(os.devnull, 'w')
 
 
+def main():
+    print("Please select your language:")
+    print("1. English")
+    print("2. Chinese")
+
+    choice = "0"
+
+    while True:
+        choice = input("Enter your choice (1 or 2): ")
+        if choice == "1":
+            print("You selected English.")
+            break
+        elif choice == "2":
+            print("You selected Chinese.")
+            break
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+    print("User input: ", choice)
+
+    if choice == "1":
+        
+        toggle_output(False)
+        # TODO: Load all models works for English
+        #  1. GLM-4 2. GPT-SoVITS 3. Wav2Lip
+        toggle_output(True)
+        print("Hello master, I am your command line private assistant powered by GLM4, GPT-SoVITS and Wav2Lip. What can I do for you today?")
+        response = "Hello master, I am your command line private assistant. What can I do for you today?"
+        # Generate the voice and lip movement
+        while True:
+            user_input = input("\nYou: ")
+            if user_input.lower() in ["exit", "quit"]:
+                break
+            response = chatbot.generate_response(user_input)
+            print("GLM-4:", response)
+
+
+
+    elif choice == "2":
+        toggle_output(False)
+        # TODO: Load all models works for Chinese
+        #  1. GLM-4 2. GPT-SoVITS 3. MuseTalk
+        toggle_output(True)
+        print("主人您好，我是您由GLM4, GPT-SoVITS and Wav2Lip驱动的的私人助理。请问今天我有什么可以帮到你？")
+        response = "主人您好，我是您的的私人助理。请问今天我有什么可以帮到你"
+        # Generate the voice and lip movement
+
+
+
+
+
+
+
+
+
+    # toggle_output(False)
+    # chatbot = GLMChatbot()
+
+    # # Begin interaction
+    # toggle_output(True)
+    # print("Welcome to the GLM-4-9B CLI chat. Type your messages below.")
+    # while True:
+    #     user_input = input("\nYou: ")
+    #     if user_input.lower() in ["exit", "quit"]:
+    #         break
+    #     response = chatbot.generate_response(user_input)
+    #     print("GLM-4:", response)
 
 
 
 if __name__ == "__main__":
-    toggle_output(False)
-    chatbot = GLMChatbot()
-
-    # Begin interaction
-    toggle_output(True)
-    print("Welcome to the GLM-4-9B CLI chat. Type your messages below.")
-    while True:
-        user_input = input("\nYou: ")
-        if user_input.lower() in ["exit", "quit"]:
-            break
-        response = chatbot.generate_response(user_input)
-        print("GLM-4:", response)
+    main()
